c00aba0870bb81eaa16b4cd5a78be4b33562bbaa
HASSAN <130797396+hassan-aamer@users.noreply.github.com>
2025-05-16 19:28:02 +0300

    fix: Update page title to use dynamic site name from settings

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -1,6 +1,6 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>@yield('title') | United creative engineers</title>
+    <title>@yield('title') | {{ setting('name') }}</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
 
