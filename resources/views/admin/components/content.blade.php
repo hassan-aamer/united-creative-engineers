dc5a3da63a93017010d2ee0ae4f56fa246ad77a7
HASSAN <130797396+hassan-aamer@users.noreply.github.com>
2025-05-19 11:13:59 +0300

    fix: Remove unnecessary div wrapper around locale content fields

--- resources/views/admin/components/content.blade.php
+++ resources/views/admin/components/content.blade.php
@@ -1,16 +1,14 @@
-<div class="row">
-    @foreach (config('app.locales') as $locale)
-        <div class="col-md-6">
-            <div class="mb-3">
-                <label class="form-label">@lang('attributes.content')
-                    ({{ __('attributes.' . $locale) }})
-                </label>
-                <textarea name="content[{{ $locale }}]" id="content_{{ $locale }}"
-                    class="form-control @error('content.' . $locale) is-invalid @enderror">{{ old('content.' . $locale, isset($result) ? $result->getTranslation('content', $locale) : '') }}</textarea>
-                @error('content.' . $locale)
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
+@foreach (config('app.locales') as $locale)
+    <div class="col-md-6">
+        <div class="mb-3">
+            <label class="form-label">@lang('attributes.content')
+                ({{ __('attributes.' . $locale) }})
+            </label>
+            <textarea name="content[{{ $locale }}]" id="content_{{ $locale }}"
+                class="form-control @error('content.' . $locale) is-invalid @enderror">{{ old('content.' . $locale, isset($result) ? $result->getTranslation('content', $locale) : '') }}</textarea>
+            @error('content.' . $locale)
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
-    @endforeach
-</div>
+    </div>
+@endforeach
