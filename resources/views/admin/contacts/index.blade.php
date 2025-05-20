72cf00ca82e45abeb9923a7aa4cabd5dc23dae67
HASSAN <130797396+hassan-aamer@users.noreply.github.com>
2025-05-20 12:13:24 +0300

    feat: Increase message display length in contacts index

--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -46,7 +46,7 @@
                                                     <td>{{ $contacts->name ?? '' }}</td>
                                                     <td>{{ $contacts->email ?? '' }}</td>
                                                     <td>{{ $contacts->phone ?? '' }}</td>
-                                                    <td>{{ shortenText($contacts->message ?? '', 50) }}</td>
+                                                    <td>{{ shortenText($contacts->message ?? '', 70) }}</td>
                                                     <td>
                                                         @can('active contacts')
                                                             <div class="form-check form-switch">
