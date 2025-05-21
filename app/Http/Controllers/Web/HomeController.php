8ab32a393a56782ddf105c90424d453ec07f9359
HASSAN <130797396+hassan-aamer@users.noreply.github.com>
2025-05-21 11:46:19 +0300

    feat: Add category selection to home page and update HomeController to include categories

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Services\Categories\CategoryService;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\Products\ProductsService;
@@ -13,17 +14,20 @@ class HomeController extends Controller
 {
     protected ProductsService $productsService;
     protected ServicesService $servicesService;
+    protected CategoryService $categoryService;
 
-    public function __construct(ProductsService $productsService, ServicesService $servicesService)
+    public function __construct(ProductsService $productsService, ServicesService $servicesService,CategoryService $categoryService)
     {
         $this->productsService = $productsService;
         $this->servicesService = $servicesService;
+        $this->categoryService = $categoryService;
     }
     public function index(Request $request)
     {
         $result = [
             'services' => $this->servicesService->index($request)->where('active',1),
             'products' => $this->productsService->index($request)->where('active',1),
+            'categories' => $this->categoryService->index(),
         ];
         return view('web.pages.home', compact('result'));
     }
--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -83,9 +83,18 @@
                 </div><!-- End Section Title -->
                 <div class="container">
                     <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+
+                        <ul class="portfolio-filters isotope-filters" data-aos="fade-up" data-aos-delay="100">
+                            <li data-filter="*" class="filter-active">All</li>
+                            @foreach ($result['categories']->sortBy('position') as $category)
+                                <li data-filter=".{{ $category->title }}">{{ $category->title ?? '' }}</li>
+                            @endforeach
+                        </ul>
+
                         <div class="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
                             @foreach ($result['products']->sortBy('position') as $product)
-                                <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-app">
+                                <div
+                                    class="col-lg-4 col-md-6 portfolio-item isotope-item {{ $product->category->title ?? '' }}">
                                     <img src="{{ App\Helpers\Image::getMediaUrl($product, 'products') }}" class="img-fluid"
                                         alt="">
                                     <div class="portfolio-info">
@@ -96,10 +105,13 @@
                                             data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i
                                                 class="bi bi-zoom-in"></i></a>
                                         <a href="{{ route('product.details', $product->id) }}" title="More Details"
-                                            class="details-link"><i class="bi bi-link-45deg"></i></a>
+                                            class="details-link">
+                                            <i class="bi bi-link-45deg"></i>
+                                        </a>
                                     </div>
                                 </div><!-- End Portfolio Item -->
                             @endforeach
+
                         </div><!-- End Portfolio Container -->
                     </div>
                 </div>
